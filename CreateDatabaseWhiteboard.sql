f72a0554f28a63afaac81e3b44265ddfe54fdfef
cynthiashlezar <cynthiashlezar@live.com>
2016-01-27 15:51:08 -0700

    database creation had extra commas. now fixed

--- CreateDatabaseWhiteboard.sql
+++ CreateDatabaseWhiteboard.sql
@@ -8,16 +8,17 @@ CREATE TABLE users (
 
 CREATE TABLE projects (
         username varchar(15),
-        project_name varchar(30)
+        project_name varchar(30),
         project_id varchar(256)
 );
 
 CREATE TABLE project_data (
+        username varchar(15),
         project_id varchar(256),
         title varchar(256),
         content varchar(500),
         x_coord int not null,
         y_coord int not null,
         type varchar(15),
-        color varchar(15),
+        color varchar(15)
 );
