e90b2f03d39771cb4d6f23495495e0132925b76f
cynthiashlezar <cynthiashlezar@live.com>
2016-01-26 22:37:46 -0700

    some files
    haven't done a whole lot. i'll add in stuff when i get the time to.

---
+++ CreateDatabaseWhiteboard.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE whiteboard;
+USE whiteboard;
+CREATE TABLE users (
+    username varchar(15),
+    password varchar(256),
+    PRIMARY KEY(username)
+);
+
+CREATE TABLE projects (
+        username varchar(15),
+        project_name varchar(30)
+        project_id varchar(256)
+);
+
+CREATE TABLE project_data (
+        project_id varchar(256),
+        title varchar(256),
+        content varchar(500),
+        x_coord int not null,
+        y_coord int not null,
+        type varchar(15),
+        color varchar(15),
+);
